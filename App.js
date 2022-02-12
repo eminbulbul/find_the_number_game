7fcd021c82527028f7422e12a2d6758f9d4e4640
m_emin <mehmeteminbul@gmail.com>
2022-02-12 19:29:58 +0300

    exercises ended

--- App.js
+++ App.js
@@ -1,1 +1,29 @@
 let holdNumber = Math.floor(Math.random() * 101);
+let takenNumber = document.getElementById('number');
+let magicHat = document.getElementById('hat');
+let result = document.getElementById('r_number');
+let attempNumber = document.getElementById('attemp');
+let counter = 0;
+let buttonCheck = document.getElementById('c_button');
+let upper = document.getElementById('upper');
+
+
+buttonCheck.addEventListener("click", () => {
+
+    if (holdNumber > Number(takenNumber.value)) {
+        upper.innerText = `! UP ! Please choose between ${takenNumber.value} - 100 `;
+        counter++;
+
+    } else if (holdNumber < Number(takenNumber.value)) {
+        upper.innerText = `! DOWN ! Please choose between 0 - ${takenNumber.value} `;
+        counter++;
+
+    } else if (holdNumber == Number(takenNumber.value)) {
+        upper.style.display = 'none';
+        counter++;
+        magicHat.style.display = 'block';
+        attempNumber.innerHTML = 'Your attempt number is : ' + counter;
+        result.innerHTML = 'Number was :' + holdNumber;
+    }
+
+});
